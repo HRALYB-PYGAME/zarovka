f9910751f766376dfac238b4c38429837b7963de
Matyas Heber <hebertmatyas@gmail.com>
2025-05-09 14:17:46 +0200

    Position.java javadoc

--- xheberm00/src/main/java/com/game/Position.java
+++ xheberm00/src/main/java/com/game/Position.java
@@ -1,22 +1,47 @@
 package com.game;
 
+/**
+ * A position on a game grid with a row and col coordinates.
+ */
 public class Position {
 	int row, col;
 
+	/**
+     * Constructs a Position with the specified row and col coordinates.
+     *
+     * @param row row coordinate
+     * @param col col coordinate
+     */
 	public Position(int row, int col) {
 		this.row = row;
 		this.col = col;
 	}
 
+	/**
+	 * Returns the row coordinate of the Position
+	 * 
+	 * @return the row coordinate
+	 */
 	public int getRow() {
 		return row;
 	}
 
+	/**
+	 * Returns the col coordinate of the Position
+	 * 
+	 * @return the col coordinate
+	 */
 	public int getCol() {
 		return col;
 	}
 
-	@Override public boolean equals(Object obj) {
+	/**
+     * Indicates whether an object is identical to the Position.
+     *
+     * @param obj object with which to compare
+     * @return {@code true} if objects are identical; {@code false} otherwise
+     */
+    @Override public boolean equals(Object obj) {
 		if (this == obj) return true;
 		if (obj == null || getClass() != obj.getClass()) return false;
 		Position position = (Position) obj;
